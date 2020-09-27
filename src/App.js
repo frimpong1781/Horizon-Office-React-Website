a1c69feddb92f427700adcad07d68b90e6dbee34
frimpong1781 <frimpongi781@yahoo.com>
2020-09-27 22:44:45 +0100

    Navbar added to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import "./style.css"
+import NavBar from './component/NavBar';
 
 function App() {
   return (
     <div>
-      
+        <NavBar />
+
     </div>
   );
 }
