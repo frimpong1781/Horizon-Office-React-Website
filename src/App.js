7ea7793e6185263c94a232110e008f3288db539a
frimpong1781 <frimpongi781@yahoo.com>
2020-09-27 23:53:37 +0100

    Add Office Type component to App.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 import "./style.css"
 import NavBar from './component/NavBar';
 import Carousel from './component/Carousel';
+import OfficeType from './component/OfficeType';
 
 function App() {
   return (
     <div>
         <NavBar />
         <Carousel />
+        <OfficeType />
     </div>
   );
 }
